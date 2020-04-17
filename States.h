5475f1b705cb32c31f5b11cbf0deee8e7b831c1d
koryakinp <koryakinp@koryakinp.com>
2020-04-17 19:57:01 -0400

    removed Serial.Test project

---
+++ States.h
@@ -0,0 +1,13 @@
+#include <WString.h>
+
+class States
+{
+  public:
+    static const String UNLOAD;
+    static const String PRE_LIFT;
+    static const String LIFT;
+    static const String THROW;
+    static const String PRE_LOWER;
+    static const String LOWER;
+    static const String PRE_UNLOAD;
+};
