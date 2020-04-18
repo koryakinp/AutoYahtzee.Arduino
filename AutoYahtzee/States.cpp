587096d48525bc94f2051556c0557df6ab6dab80
koryakinp <koryakinp@koryakinp.com>
2020-04-17 20:00:30 -0400

    clean up

--- AutoYahtzee/States.cpp
+++
@@ -1,9 +0,0 @@
-#include "States.h"
-
-const String States::UNLOAD = "Unload";
-const String States::PRE_LIFT = "PreLift";
-const String States::LIFT = "Lift";
-const String States::THROW = "Trow";
-const String States::PRE_LOWER = "PreLower";
-const String States::LOWER = "Lower";
-const String States::PRE_UNLOAD = "PreUnload";
